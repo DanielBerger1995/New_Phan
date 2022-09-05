91d660154b4a9ace276bc6099c10b91376e07f68
DanielBerger1995 <55579357+DanielBerger1995@users.noreply.github.com>
2022-09-05 09:20:53 +0200

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,7 +1,7 @@
 
-/*
+
         $(document).ready(function(){
-        $(function() {
+        
             $('a[href*=\\#]:not([href=\\#])').on('click', function() {
                 
               
@@ -29,55 +29,10 @@
             
             });
            
-        });
+       
       });
      
-*/
 
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    if ($(window).width() < 950) {
-      var customOffset = 64;
-    }
- 
-
-
-    else {
-        var customOffset = 70;
-    }
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top - customOffset
-        }, 300, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
 
 $(document).ready(function(){
     $(window).scroll(function(){
